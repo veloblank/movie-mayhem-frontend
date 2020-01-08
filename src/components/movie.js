0ced648552f775140c98f31a49fc907cd0456a99
John Blank <veloblank@gmail.com>
2020-01-08 04:33:28 -0500

    Build class Movie with constructor

---
+++ src/components/movie.js
@@ -0,0 +1,10 @@
+class Movie {
+  constructor(object) {
+    this.title = object.title;
+    this.synopsis = object.synopsis;
+    this.rating = object.rating;
+    this.poster_src = object.poster_src;
+  }
+}
+
+export default Movie;
