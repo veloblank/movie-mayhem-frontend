ef7c80f300effd37bd82731c9f06d7c15fcdc3c6
John Blank <veloblank@gmail.com>
2020-01-08 04:33:59 -0500

    Simple style and create render() movie

--- src/components/movies.js
+++ src/components/movies.js
@@ -1,4 +1,5 @@
 import MoviesAdapter from "../adapters/MoviesAdapter.js";
+import Movie from "./movie.js";
 
 export default class Movies {
   constructor() {
@@ -8,8 +9,31 @@ export default class Movies {
   }
 
   fetchAndLoadMovies() {
-    this.adapter.getMovies().then(movies => {
-      console.log(movies);
-    });
+    this.adapter
+      .getMovies()
+      .then(movies => {
+        movies.forEach(movie => this.movies.push(new Movie(movie)));
+      })
+      .then(() => this.render());
+  }
+
+  render(movie) {
+    let moviesContainer = document.getElementById("movies-list");
+    for (let movie of this.movies) {
+      let movieDiv = document.createElement("div");
+      let moviePoster = document.createElement("img");
+      let movieTitle = document.createElement("div");
+      let movieSynopsis = document.createElement("p");
+
+      movieDiv.classList.add("movie-card");
+      moviePoster.src = movie.poster_src;
+      movieTitle.innerHTML = movie.title;
+      movieSynopsis.innerHTML = movie.synopsis;
+
+      moviesContainer.appendChild(movieDiv);
+      movieDiv.appendChild(moviePoster);
+      movieDiv.append(movieTitle);
+      movieTitle.append(movieSynopsis);
+    }
   }
 }
